32df895ce31c24d754b5836a9148678dc6695bc1
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-18 16:12:41 -0600

    Actualizar estilos y contenido en componentes CardTestimony, WhyWe y Hero

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,13 +1,38 @@
-
   //Hero
-  import React from 'react';
+  
+'use client';
 import styles from './Hero.module.css';
-import { FcServices } from "react-icons/fc";
 import Link from 'next/link';
 import Image from 'next/image';
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import React, { useState, useEffect } from 'react';
+
+
+
 
 
 export const Hero = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+        // Activa 'isScrolled' si el scroll es mayor que un cierto número de píxeles
+        // Puedes ajustar '50' a cualquier otro número basado en tu necesidad
+        setIsScrolled(window.scrollY > 50);
+    };
+
+    // Escucha al evento de scroll
+    window.addEventListener('scroll', handleScroll);
+
+    // Limpieza del event listener
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
   return (
     <>
     <div className={styles.Hero__container}>
@@ -15,11 +40,11 @@ export const Hero = () => {
       <div className={styles.Hero__container__text}>
       
       <div className={styles.Hero__container__text__title}>
-        <h1>Transformación Digital Corporativa <span className={styles.WaterEffect}>Organizaciónal</span><span className={styles.Dot}>.</span></h1>
+        <h1>Transformación Digital Corporativa<span className={styles.Dot}>.</span></h1>
       </div>
 
       <div className={styles.Hero__container__text__subtitle}>
-        <p>Expertos en software que facilitan tu transformación digital y gestionan el cambio, garantizando soluciones innovadoras y resultados duraderos.</p>
+        <p>Expertos en soluciones de software que facilitan tu transformación digital y gestionan el cambio, garantizando innovación y resultados duraderos.</p>
       </div>
       <div className={styles.Hero__container__text__form}>
         <form action="" className={styles.Hero__container__text__form__container}>
@@ -36,11 +61,16 @@ export const Hero = () => {
       </div>
 
       <div className={styles.Hero__container__image}>
-        <Image src="/hero.svg" alt="Hero" width={500} height={500}/>
+        <Image src="/hero.svg" alt="Hero" width={700} height={400}/>
 
       </div>
 
     </div>
+    <div className={`${styles.Hero__container__row__down__animated} ${isScrolled ? styles.scrolled : ''}`}>
+        <MdOutlineKeyboardArrowDown className={`${styles.Hero__container__row__down__animated__icon} ${isScrolled ? styles.iconScrolled : ''}`}/>
+    </div>
+
+
     
 
     </>
--- components/WhyWe/WhyWe.jsx
+++ components/WhyWe/WhyWe.jsx
@@ -8,15 +8,16 @@ export const WhyWe = () => {
   return (
     <div className={styles.WhyWe__container}>
 
-      <div className={styles.WhyWe__container_text}>
-          <h2>¿Por qué nosotros?</h2>
-          <p>Porque somos los mejores</p>
-        </div>
+      
 
         <div className={styles.WhyWe__container_images}>
           <Image src="logo.svg"  alt="wyhywephoto" width={200} height={200} />
         </div>
-  
+        
+        <div className={styles.WhyWe__container_text}>
+          <h2>¿Por qué nosotros?</h2>
+          <p>Porque somos los mejores</p>
+        </div>
       
     </div>
   );
