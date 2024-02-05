07887b1279e24cb843950278663d80d4be4ff035
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-04 20:47:23 -0600

    Actualizar Dockerfile.dev y next.config.js, agregar dependencia child_process en package-lock.json y package.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['csm.queb.online'], // Añade el dominio de tus imágenes aquí
-    },
-    // ... otras configuraciones si las tienes
-  }
-  
-  module.exports = nextConfig;
-  
+  images: {
+    domains: ['csm.queb.online'], // Añade el dominio de tus imágenes aquí
+  },
+  // ... otras configuraciones si las tienes
+  generateBuildId: async () => {
+    // Obtener el hash más reciente de git
+    const { stdout } = await require('child_process').exec('git rev-parse HEAD')
+    const gitHash = stdout.trim()
+
+    return gitHash
+  },
+};
+
+module.exports = nextConfig;
