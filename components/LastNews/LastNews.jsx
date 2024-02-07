10f4e151cea401b42407ce11064771dfb64d1719
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-07 04:07:42 -0600

    Actualiza estilos de CSS en módulos y componentes

--- components/LastNews/LastNews.jsx
+++ components/LastNews/LastNews.jsx
@@ -1,49 +1,49 @@
+//LastNews
+import React from 'react';
+import styles from './LastNews.module.css';
+import { CardPost } from '../CardPost/CardPost';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from 'next/link'
 
-  //LastNews
-  import React from 'react';
-  import styles from './LastNews.module.css';
-  import { CardPost } from '../CardPost/CardPost';
-  import fs from 'fs'
-  import path from 'path'
-  import matter from 'gray-matter'
-  import Link from 'next/link'
-  
-  export const LastNews = () => {
-    const blogDir  = "blogs";
-  
-    const files = fs.readdirSync(path.join(blogDir));
-  
-    const blogs =  files.map(filename =>{
-      
-      const fileContent = fs.readFileSync(path.join(blogDir, filename), 'utf-8')
-  
-  
-      const {data: frontMatter} = matter(fileContent);
-      return {
-        meta: frontMatter,
-        slug: filename.replace('.mdx', '')
-      }
-  
-    })
-  
-  return (
-    <>
-    <div className={styles.LastNews__container__title}>
-      <h3 className={styles.LastNews__title}>Últimos Artículos</h3>
+export const LastNews = () => {
+  const blogDir  = "blogs";
+
+  const files = fs.readdirSync(path.join(blogDir));
+
+  const blogs =  files.map(filename =>{
     
-    </div>
-    <div className={styles.LastNews__container}>
-    { blogs.map(blog => (
-        <Link href={`/blogs/${blog.slug}`} key={blog.slug} style={{color: 'white',}}>
-           <CardPost Titulo={blog.meta.title} Description={blog.meta.description} ImagePost={blog.meta.image}  />
-        </Link>
-      ))}
-     
+    const fileContent = fs.readFileSync(path.join(blogDir, filename), 'utf-8')
 
-      
-    </div>
-    </>
-  );
+
+    const {data: frontMatter} = matter(fileContent);
+    return {
+      meta: frontMatter,
+      slug: filename.replace('.mdx', '')
+    }
+
+  })
+
+return (
+  <>
+  
+  <div className={styles.LastNews__container__title}>
+    <h3 className={styles.LastNews__title}>Últimos Artículos</h3>
+  
+  </div>
+  <div className={styles.LastNews__container}>
+  { blogs.map(blog => (
+      <Link href={`/blogs/${blog.slug}`} key={blog.slug} style={{color: 'white',}}>
+         <CardPost Titulo={blog.meta.title} Description={blog.meta.description} ImagePost={blog.meta.image}  />
+      </Link>
+    ))}
+   
+
+    
+  </div>
+  </>
+);
 }
 
 export default LastNews;
