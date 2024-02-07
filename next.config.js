8066a8bb866fbb7a1011f674726b93cdbefb1b6a
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-07 13:17:28 -0600

    Actualizar next.config.js para usar la opción "output: standalone"

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone'
+}
 
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-
-module.exports = {
-  images: {
-    domains: ['csm.queb.online'],
-  },
-  generateBuildId: async () => {
-    try {
-      const { stdout, stderr } = await exec('git rev-parse HEAD');
-      if (stderr) {
-        console.error(stderr);
-        throw new Error(stderr);
-      }
-      return stdout.trim();
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  },
-};
+module.exports = nextConfig
