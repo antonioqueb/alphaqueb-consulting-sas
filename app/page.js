197125828cdcad6f84a8b347486ee809de541128
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-07 01:41:13 -0600

    Actualizar estilos y eliminar blogs innecesarios

--- app/page.js
+++ app/page.js
@@ -3,30 +3,8 @@ import Hero from '../components/Hero/Hero'
 import Testimonials from '../components/Testimonials/Testimonials'
 import WhyWe from '../components/WhyWe/WhyWe'
 import Methodology from '../components/Methodology/Methodology'
-import Image from 'next/image'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import Link from 'next/link'
-
 
 export default function Home() {
-  const blogDir  = "blogs";
-
-  const files = fs.readdirSync(path.join(blogDir));
-
-  const blogs =  files.map(filename =>{
-    
-    const fileContent = fs.readFileSync(path.join(blogDir, filename), 'utf-8')
-
-
-    const {data: frontMatter} = matter(fileContent);
-    return {
-      meta: frontMatter,
-      slug: filename.replace('.mdx', '')
-    }
-
-  })
 
 
   return (
@@ -38,20 +16,6 @@ export default function Home() {
      
         
       <Hero /> 
-
-      
-
-     { blogs.map(blog => (
-        <Link href={`/blogs/${blog.slug}`} key={blog.slug} style={{color: 'white',}}>
-          <div>
-          <h2>{blog.meta.title}</h2>
-          <p>{blog.meta.description}</p>
-          </div>
-        
-          
-          
-        </Link>
-      ))}
 
 
  
--- components/LastNews/LastNews.jsx
+++ components/LastNews/LastNews.jsx
@@ -3,8 +3,29 @@
 import React from 'react';
 import styles from './LastNews.module.css';
 import { CardPost } from '../CardPost/CardPost';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from 'next/link'
 
 export const LastNews = () => {
+  const blogDir  = "blogs";
+
+  const files = fs.readdirSync(path.join(blogDir));
+
+  const blogs =  files.map(filename =>{
+    
+    const fileContent = fs.readFileSync(path.join(blogDir, filename), 'utf-8')
+
+
+    const {data: frontMatter} = matter(fileContent);
+    return {
+      meta: frontMatter,
+      slug: filename.replace('.mdx', '')
+    }
+
+  })
+
   return (
     <>
     <div className={styles.LastNews__container__title}>
@@ -12,12 +33,12 @@ export const LastNews = () => {
     
     </div>
     <div className={styles.LastNews__container}>
-      
-      <CardPost Titulo="Dominando la Comprensión Profunda en la Transformación Digital" Description="Descubre cómo un análisis exhaustivo puede ser el catalizador de una estrategia digital personalizada. Exploramos la importancia de entender cada faceta de tu negocio, desde la operación diaria hasta tus metas a largo plazo, y cómo esto establece un camino claro hacia el crecimiento y la innovación." ImagePost="/erp.webp" />
-      <CardPost Titulo="¿Estrategias de Planeación Digital para un Futuro Escalable?" Description="Adéntrate en el arte de la planeación digital y cómo seleccionar las tecnologías y procesos más adecuados puede transformar tu negocio. Aprende sobre la creación de un plan flexible y evolutivo, diseñado para escalar con tu empresa y garantizar un progreso " ImagePost="/automatizacion.jpg"/>
-      <CardPost Titulo="La Clave de una Integración Impecable en Sistemas Digitales" Description="Descubre cómo una integración perfecta puede cambiar el juego en tu transformación digital. Profundizamos en la importancia de fusionar soluciones con tu infraestructura existente, optimizando procesos y potenciando la eficiencia, y cómo asegurar una transición fluida que fortalezca tu negocio." ImagePost="/rentabilidad.jpg"/>
-      <CardPost Titulo="Empoderamiento Digital: Capacitando Equipos para el Futuro" Description="Explora el poder del empoderamiento digital y cómo un equipo bien capacitado puede ser tu mayor activo. Hablamos sobre estrategias para equipar a tus empleados con las herramientas necesarias para maximizar el potencial de las nuevas tecnologías y garantizar el éxito continuo de tu transformación digital." ImagePost="/sistematizar.png"/>
-
+    { blogs.map(blog => (
+        <Link href={`/blogs/${blog.slug}`} key={blog.slug} style={{color: 'white',}}>
+           <CardPost Titulo={blog.meta.title} Description={blog.meta.description} ImagePost={blog.meta.image}  />
+        </Link>
+      ))}
+     
 
       
     </div>
