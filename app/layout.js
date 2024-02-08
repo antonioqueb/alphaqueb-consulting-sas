82ae7addc7dcbbe35165f1029662d293dc2357b3
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-07 20:38:03 -0600

    Corregir error en función gtag() en layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HFW2B50CLE"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
+      function gtag(){dataLayer.push(arguments)};
       gtag('js', new Date());
 
       gtag('config', 'G-HFW2B50CLE');
