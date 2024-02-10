2d3f04240a98eec2247d705eea42ca6e4dff68ed
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-09 18:13:43 -0600

    Update

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,39 @@
-import { Rubik } from 'next/font/google'
-import './globals.css'
-import NavbarComponent from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer'
+import { Rubik } from 'next/font/google';
+import Script from 'next/script';
+import './globals.css';
+import NavbarComponent from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+
+const rubik = Rubik({ subsets: ['latin'] });
 
 
-const rubik = Rubik ({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Transformación Digital',
-  description: 'Transformación Digital, expertos en tecnología y desarrollo de software',
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-    
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HFW2B50CLE"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments)};
-              gtag('js', new Date());
-              gtag('config', 'G-HFW2B50CLE');
-            `,
-          }}
+      <head>
+        {/* En esta sección puedes incluir otras etiquetas relacionadas a  <head>
+         como los metadatos, títulos, links  */}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HFW2B50CLE"
+          strategy="afterInteractive"
         />
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)};
+            gtag('js', new Date());
+            gtag('config', 'G-HFW2B50CLE');
+          `}
+        </Script>
+      </head>
+
       <body className={rubik.className}>
         <NavbarComponent />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
