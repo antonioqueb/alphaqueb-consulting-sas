3aff099806bbb33c6337cfe18307d377d18bddb8
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-09 18:13:43 -0600

    Update

--- app/page.js
+++ app/page.js
@@ -4,7 +4,13 @@ import Testimonials from '../components/Testimonials/Testimonials'
 import WhyWe from '../components/WhyWe/WhyWe'
 import Methodology from '../components/Methodology/Methodology'
 
+export const metadata = {
+  title: 'Transformación Digital',
+  description: 'Transformación Digital, expertos en tecnología y desarrollo de software',
+};
+
 export default function Home() {
+
 
 
   return (
