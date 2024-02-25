a99c339baab8d14cb0838422031a56b99d8d6156
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-25 12:37:52 -0600

    Up

--- data/Testimonials.js
+++ data/Testimonials.js
@@ -1,15 +1,17 @@
 [
   {
+    id: 1,
     imageSrc: '/logo.svg',
     altText: 'Imagen de John Doe',
     testimony: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisquam, voluptatibus.',
     name: 'John Doe'
   },
   {
+    id: 2,
     imageSrc: '/logo.svg',
     altText: 'Imagen de Jane Doe',
     testimony: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
     name: 'Jane Doe'
   }
-  // ... más testimonios
+ 
 ]
