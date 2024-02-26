d72a3bd85fdc981847e7cdeac63bc9a6656642c0
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-26 17:43:47 -0600

    Update

--- components/Methodology/Methodology.jsx
+++ components/Methodology/Methodology.jsx
@@ -20,10 +20,7 @@ export default function Methodology () {
              <h4 className='text-2xl my-2'><strong>En la etapa de Descubrimiento:</strong></h4>
              <p className='text-gray-300'><strong >Conoceremos tu implementación:</strong > Entenderemos tus necesidades, objetivos y procesos para desarrollar una solución personalizada.</p>
              <p className='text-gray-300'><strong>Definiremos la aplicación:</strong> Trabajaremos contigo para identificar las funcionalidades clave y el plan de desarrollo.</p>
-             <h4 className='text-2xl my-2'><strong>El resultado:</strong></h4>
-             <p className='text-gray-300'><strong>Plan y presupuesto:</strong> Te presentaremos una propuesta detallada con objetivos, funcionalidades, tecnología y cronograma.</p>
-             <p className='text-gray-300'><strong>Confianza y experiencia:</strong> Te guiaremos en cada paso del proceso, brindándote la confianza de que tu proyecto está en buenas manos.</p>
-          </div>
+             </div>
           </div>
           <div className={styles.Methodology__container_steps_step_right}>
          <div className={styles.Methodology__container_steps_step_image}>
@@ -36,11 +33,7 @@ export default function Methodology () {
           <p className='text-gray-300'><strong >Funcionalidad sobre diseño:</strong > Nos centramos en crear una interfaz intuitiva que priorice la funcionalidad del módulo.</p>
           <p className='text-gray-300'><strong>Aprovechamiento del diseño estándar de Odoo:</strong> Mantenemos la familiaridad y usabilidad de la plataforma.</p>
 
-          <h4 className='text-2xl my-2'><strong>El resultado:</strong></h4>
-
-          <p className='text-gray-300'><strong >Módulo Odoo funcional y eficiente:</strong > Tu módulo tendrá una interfaz intuitiva que facilita el uso de las funcionalidades.</p>
-          <p className='text-gray-300'><strong>Experiencia de usuario optimizada:</strong> La navegación será fluida y los usuarios podrán encontrar lo que buscan rápidamente.</p>
-
+         
           </div>
           </div>
           <div className={styles.Methodology__container_steps_step_left}>
@@ -54,9 +47,6 @@ export default function Methodology () {
           <p className='text-gray-300'><strong >Tecnologías y frameworks de vanguardia:</strong >  Implementamos las últimas tecnologías y frameworks de Odoo para construir una aplicación robusta y escalable.</p>
           <p className='text-gray-300'><strong>Comunicación constante:</strong> Te mantenemos informado del progreso del desarrollo mediante comunicaciones constantes, asegurando que el proyecto esté alineado con tus expectativas.</p>
     
-          <h4 className='text-2xl my-2'><strong>El resultado:</strong></h4>
-
-          <p className='text-gray-300'><strong >Aplicación robusta y escalable:</strong > Tu aplicación estará preparada para crecer junto a tu negocio, soportando un alto volumen de usuarios y datos.</p>
           </div>
           </div>
           <div className={styles.Methodology__container_steps_step_right}>
@@ -70,11 +60,7 @@ export default function Methodology () {
           <p className='text-gray-300'><strong >Asistencia en la implementación:</strong > Te ayudaremos a implementar la aplicación en tu entorno de producción, asegurándonos de que la transición sea fluida y sin interrupciones.</p>
           <p className='text-gray-300'><strong>Capacitación personalizada:</strong> Ofreceremos capacitación y soporte técnico personalizado a tu equipo para que puedan usar la aplicación de manera eficiente, aprovechando al máximo todas sus funcionalidades.</p>
     
-          <h4 className='text-2xl my-2'><strong>El resultado:</strong></h4>
-
-          <p className='text-gray-300'><strong >Despliegue sin problemas:</strong > Tu aplicación estará lista para usarse en tu entorno de producción, sin interrupciones en tu flujo de trabajo.</p>
-          <p className='text-gray-300'><strong >Equipo capacitado:</strong > Tu equipo estará capacitado para usar la aplicación de manera eficiente, maximizando su productividad.</p>
-           </div>
+          </div>
           </div>
           <div className={styles.Methodology__container_steps_step_left}>
          <div className={styles.Methodology__container_steps_step_image}>
@@ -87,11 +73,7 @@ export default function Methodology () {
           <p className='text-gray-300'><strong >Mejora continua:</strong > Continuaremos trabajando contigo para mejorar la aplicación y adaptarla a las nuevas necesidades de tu negocio, asegurando que siempre esté alineada con tus objetivos.</p>
           <p className='text-gray-300'><strong>Mantenimiento y seguridad:</strong> Te ayudaremos a mantener la aplicación actualizada y segura, protegiendo tu información y garantizando su correcto funcionamiento.</p>
     
-          <h4 className='text-2xl my-2'><strong>El resultado:</strong></h4>
-
-          <p className='text-gray-300'><strong >Aplicación siempre actualizada:</strong > Tu aplicación siempre estará al día con las últimas tecnologías y funcionalidades de Odoo.</p>
-          <p className='text-gray-300'><strong >Adaptación a tus necesidades:</strong > La aplicación se adaptará a las nuevas necesidades de tu negocio, permitiéndote crecer y evolucionar sin limitaciones.</p>
-             </div>
+         </div>
           </div>
       </div>
 
