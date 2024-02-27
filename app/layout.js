1cb5b45d5d80fa2d00921894741fd164ee613049
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-27 16:06:43 -0600

    Up

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,17 @@ export default function RootLayout ({ children }) {
             `,
           }}
         />
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M57HBTQP');</script>
+            `,
+          }}
+        />
   
     <body className={rubik.className}>
 
@@ -30,6 +41,8 @@ export default function RootLayout ({ children }) {
   <div className="container-layout">
       <div className="glass max-w-[1200px] mx-auto px-[2rem] xl:px-[8rem]">
       <NavbarComponent/>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M57HBTQP"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
     
      {children}
       
