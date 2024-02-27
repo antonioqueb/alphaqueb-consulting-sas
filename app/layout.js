094fb405b41352407bdd503437e04a3d9ffdafa9
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-26 19:17:21 -0600

    Up background

--- app/layout.js
+++ app/layout.js
@@ -8,13 +8,27 @@ const rubik = Rubik({ subsets: ['latin'] })
 export default function RootLayout ({ children }) {
   return (
     <html lang="es">
+  
+    <body className={rubik.className}>
+    
+  <div className="container-layout">
+      <div className="glass">
+      <NavbarComponent />
+     
+     {children}
       
-
-      <body className={rubik.className}>
-        <NavbarComponent />
-        {children}
-        <Footer />
+     <Footer/>
+      </div>
+      <div className="moving-balls">
+          <div className="ball small-day"></div>
+          <div className="ball medium"></div>
+          <div className="ball ultra-tiny"></div>
+          <div className="ball mini"></div>
+          <div className="ball giant"></div>
+      </div>
+  </div>
       </body>
-    </html>
+    
+  </html>
   )
 }
