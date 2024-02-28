d3373b67e801f6a7ddc297ac7d0025a955655fba
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-28 15:26:39 -0600

    Up

--- app/page.js
+++ app/page.js
@@ -14,7 +14,8 @@ export const metadata = {
     default: 'Apps Odoo Personalizadas',
     template: '%s | Alphaqueb'
   },
-  description: 'Personalizamos tu implementación odoo para cumplir tus metas'
+  description: 'Personalizamos tu implementación odoo para cumplir tus metas',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL)
  
 }
 
