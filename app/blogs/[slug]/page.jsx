f2ce8f3a013ebbd776b0bb14253f4a2e4fcdb99c
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-28 12:30:18 -0600

    Up

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -33,16 +33,16 @@ export default function Page({ params }) {
 
     return (
         <>
-             <head>
-            
-            {props.frontMatter.title ? (
-            <title>{props.frontMatter.title}</title>
-            ) : (
-            <title>Desarrollo Apps Odoo</title>
-            )}
-            <meta name="description" content={props.frontMatter.description} />
-            <meta property="og:image" content={props.frontMatter.image} />
-        </head>
+            <head>
+
+                {props.frontMatter.title ? (
+                    <title>{props.frontMatter.title}</title>
+                ) : (
+                    <title>Desarrollo Apps Odoo</title>
+                )}
+                <meta name="description" content={props.frontMatter.description} />
+                <meta property="og:image" content={props.frontMatter.image} />
+            </head>
             <article className={styles.main}>
                 <h1>{props.frontMatter.title}</h1>
                 <Image src={props.frontMatter.image} alt={props.frontMatter.title} width={1024} height={1024} className={styles['image-cover']} />
