5523062ee7050f1489fa8e88babcdd50093aad15
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-03-05 11:01:25 -0600

    Up

--- components/Plans/Plans.js
+++ components/Plans/Plans.js
@@ -35,7 +35,7 @@ const NavbarComponent = () => {
      
         <div className='w-full mx-auto mb-14 text-start lg:text-center'>
             <h1 className='text-[1.8rem] font-semibold mb-6 lg:text-5xl xl:text-6xl'>Tarifas por Hora<span className="text-[#26E1FF]">.</span></h1>
-            <p className='text-xl text-gray-100 lg:text-[1.5rem]'>Desde ajustes rápidos hasta proyectos integrales, tenemos una solución a tu medida. Todos nuestros precios son transparentes e incluyen IVA.</p>
+            <p className='text-xl text-gray-100 lg:text-[1.5rem]'>Desde ajustes rápidos hasta proyectos integrales, tenemos una solución a tu medida. Todos nuestros precios son transparentes e incluyen IVA. Ahorro respecto al paquete de 2 horas.</p>
         </div>
         
         <div className='flex flex-col justify-between items-center lg:flex-row lg:items-start'>
