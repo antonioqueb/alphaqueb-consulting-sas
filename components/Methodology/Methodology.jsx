8702bb8a77fa3bb34656019bd786e5587e272434
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-03-05 04:17:25 -0600

    Update

--- components/Methodology/Methodology.jsx
+++ components/Methodology/Methodology.jsx
@@ -25,10 +25,10 @@ export default function Methodology () {
           Requerimiento de personalización Odoo<span className="text-[#26E1FF]">.</span>
         </h2>
 
-          <h4 className='text-2xl mb-3 sm:text-3xl'><strong>Etapa de descubrimiento:</strong></h4>
+          <h4 className='text-2xl mb-3 sm:text-[1.1rem]'><strong>Etapa de descubrimiento:</strong></h4>
 
-         <p className='text-gray-300 text-2xl pb-3 sm:text-3xl'><strong >Conoceremos tu implementación:</strong > Entenderemos tus necesidades, objetivos y procesos para desarrollar una solución personalizada.</p>
-         <p className='text-gray-300 text-2xl sm:text-3xl'><strong>Definiremos la aplicación:</strong> Trabajaremos contigo para identificar las funcionalidades clave y el plan de desarrollo.</p>
+         <p className='text-gray-300 text-2xl pb-3 sm:text-[1.1rem]'><strong >Conoceremos tu implementación:</strong > Entenderemos tus necesidades, objetivos y procesos para desarrollar una solución personalizada.</p>
+         <p className='text-gray-300 text-2xl sm:text-[1.1rem]'><strong>Definiremos la aplicación:</strong> Trabajaremos contigo para identificar las funcionalidades clave y el plan de desarrollo.</p>
           </div>     
     </div>
 
@@ -47,10 +47,10 @@ export default function Methodology () {
         Diseño Odoo Funcional<span className="text-[#26E1FF]">.</span>
         </h2>
 
-          <h4 className='text-2xl mb-3 sm:text-3xl'><strong>Etapa de diseño:</strong></h4>
+          <h4 className='text-2xl mb-3 sm:text-[1.1rem]'><strong>Etapa de diseño:</strong></h4>
 
-         <p className='text-gray-300 text-2xl pb-3 sm:text-3xl'><strong >Funcionalidad sobre diseño:</strong > Nos centramos en crear una interfaz intuitiva que priorice la funcionalidad del módulo.</p>
-         <p className='text-gray-300 text-2xl sm:text-3xl'><strong>Aprovechamiento del diseño estándar de Odoo:</strong> Mantenemos la familiaridad y usabilidad de la plataforma.</p>
+         <p className='text-gray-300 text-2xl pb-3 sm:text-[1.1rem]'><strong >Funcionalidad sobre diseño:</strong > Nos centramos en crear una interfaz intuitiva que priorice la funcionalidad del módulo.</p>
+         <p className='text-gray-300 text-2xl sm:text-[1.1rem]'><strong>Aprovechamiento del diseño estándar de Odoo:</strong> Mantenemos la familiaridad y usabilidad de la plataforma.</p>
           </div>     
     </div>
 
@@ -74,10 +74,10 @@ export default function Methodology () {
         Desarrollo de software Odoo<span className="text-[#26E1FF]">.</span>
         </h2>
 
-          <h4 className='text-2xl mb-3 sm:text-3xl'><strong>Etapa de desarrollo:</strong></h4>
+          <h4 className='text-2xl mb-3 sm:text-[1.1rem]'><strong>Etapa de desarrollo:</strong></h4>
 
-         <p className='text-gray-300 text-2xl pb-3 sm:text-3xl'><strong >Tecnologías y frameworks de vanguardia:</strong > Implementamos las últimas tecnologías y frameworks de Odoo para construir una aplicación robusta y escalable.</p>
-         <p className='text-gray-300 text-2xl sm:text-3xl'><strong>Comunicación constante:</strong> Te mantenemos informado del progreso del desarrollo mediante comunicaciones constantes, asegurando que el proyecto esté alineado con tus expectativas.</p>
+         <p className='text-gray-300 text-2xl pb-3 sm:text-[1.1rem]'><strong >Tecnologías y frameworks de vanguardia:</strong > Implementamos las últimas tecnologías y frameworks de Odoo para construir una aplicación robusta y escalable.</p>
+         <p className='text-gray-300 text-2xl sm:text-[1.1rem]'><strong>Comunicación constante:</strong> Te mantenemos informado del progreso del desarrollo mediante comunicaciones constantes, asegurando que el proyecto esté alineado con tus expectativas.</p>
           </div>     
     </div>
 
@@ -106,10 +106,10 @@ export default function Methodology () {
         Implementación tu solución personalizada Odoo<span className="text-[#26E1FF]">.</span>
         </h2>
 
-          <h4 className='text-2xl mb-3 sm:text-3xl'><strong>Etapa de implementación:</strong></h4>
+          <h4 className='text-2xl mb-3 sm:text-[1.1rem]'><strong>Etapa de implementación:</strong></h4>
 
-         <p className='text-gray-300 text-2xl pb-3 sm:text-3xl'><strong >Asistencia en la implementación:</strong > Te ayudaremos a implementar la aplicación en tu entorno de producción, asegurándonos de que la transición sea fluida y sin interrupciones.</p>
-         <p className='text-gray-300 text-2xl sm:text-3xl'><strong>Capacitación personalizada:</strong> Ofreceremos capacitación y soporte técnico personalizado a tu equipo para que puedan usar la aplicación de manera eficiente, aprovechando al máximo todas sus funcionalidades.</p>
+         <p className='text-gray-300 text-2xl pb-3 sm:text-[1.1rem]'><strong >Asistencia en la implementación:</strong > Te ayudaremos a implementar la aplicación en tu entorno de producción, asegurándonos de que la transición sea fluida y sin interrupciones.</p>
+         <p className='text-gray-300 text-2xl sm:text-[1.1rem]'><strong>Capacitación personalizada:</strong> Ofreceremos capacitación y soporte técnico personalizado a tu equipo para que puedan usar la aplicación de manera eficiente, aprovechando al máximo todas sus funcionalidades.</p>
           </div>     
     </div>
 
@@ -140,10 +140,10 @@ export default function Methodology () {
         Evoluciónamos nuestra solución personalizada Odoo<span className="text-[#26E1FF]">.</span>
         </h2>
 
-          <h4 className='text-2xl mb-3 sm:text-3xl'><strong>Etapa de evolución:</strong></h4>
+          <h4 className='text-2xl mb-3 sm:text-[1.1rem]'><strong>Etapa de evolución:</strong></h4>
 
-         <p className='text-gray-300 text-2xl pb-3 sm:text-3xl'><strong >Mejora continua:</strong > Continuaremos trabajando contigo para mejorar la aplicación y adaptarla a las nuevas necesidades de tu negocio, asegurando que siempre esté alineada con tus objetivos.</p>
-         <p className='text-gray-300 text-2xl sm:text-3xl'><strong>Mantenimiento y seguridad:</strong> Te ayudaremos a mantener la aplicación actualizada y segura, protegiendo tu información y garantizando su correcto funcionamiento.</p>
+         <p className='text-gray-300 text-2xl pb-3 sm:text-[1.1rem]'><strong >Mejora continua:</strong > Continuaremos trabajando contigo para mejorar la aplicación y adaptarla a las nuevas necesidades de tu negocio, asegurando que siempre esté alineada con tus objetivos.</p>
+         <p className='text-gray-300 text-2xl sm:text-[1.1rem]'><strong>Mantenimiento y seguridad:</strong> Te ayudaremos a mantener la aplicación actualizada y segura, protegiendo tu información y garantizando su correcto funcionamiento.</p>
           </div>     
     </div>
 
