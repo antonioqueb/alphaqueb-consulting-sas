6651717b439b2f1d873b1bd77a4709d9eed2b314
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-03-14 06:20:46 -0600

    Update hero

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -61,7 +61,7 @@ const Hero = () => {
       <section className='pt-1 lg:pt-20 pb-12 bg-transparent mt-[4.5rem]'>
         <div className='mx-auto'>
           <div className='mx-auto text-center'>
-            <h1 className='mb-8 text-[1.5rem] sm:text-[2.4rem] font-semibold leading-none text-white xl:text-[3.5rem] 2xl:text-[4.65rem] md:tracking-tight text-balance hover:shadow-xl transition-all duration-300 custom-text-size xl:text-[3.4rem]'><span className='text-blueprimary text-[1.5rem]  2xl:text-[5rem] sm:text-[2.6rem] xl:text-[3.7rem]'>Innovación y Crecimiento con Odoo: </span>Implementamos la solución integral de CRM y ERP en español para tu negocio.
+            <h1 className='mb-8 text-[1.5rem] sm:text-[2.4rem] font-semibold leading-none text-white 2xl:text-[4.65rem] md:tracking-tight text-balance hover:shadow-xl transition-all duration-300 custom-text-size xl:text-[3.4rem]'><span className='text-blueprimary text-[1.5rem]  2xl:text-[5rem] sm:text-[2.6rem] xl:text-[3.7rem]'>Innovación y Crecimiento con Odoo: </span>Implementamos la solución integral de CRM y ERP en Español para tu negocio.
             </h1>
             <h2 className='text-balance leading-normal w-full mb-8 text-xl text-white md:text-[1.45rem] xl:text-[2.1rem]  2xl:text-[1.48rem]'>
             <strong>Evaluación Personalizada Sin Costo:</strong> Descubre cómo Odoo puede revolucionar tu empresa. ¡Reclama tu evaluación hoy!
