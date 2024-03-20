5d5042e2e56a2823784c9385ebb60e98fb95ae13
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-03-20 02:29:47 -0600

    Up

--- components/WhyWe/WhyWe.jsx
+++ components/WhyWe/WhyWe.jsx
@@ -6,7 +6,7 @@ export default function WhyWe() {
     <div className="flex justify-center items-center flex-col md:flex-col-reverse lg:flex-row text-[#F2F2F2] py-10 md:py-20 lg:py-10">
 
       <div className="flex-1 flex justify-center">
-        <Image className="w-full h-auto object-contain md:max-w-xs lg:max-w-lg mx-auto my-8" src='whywe.svg' alt='wyhywephoto' width={500} height={500} />
+        <Image className="w-full h-auto object-contain md:max-w-xs lg:max-w-lg mx-auto my-8" src='/whywe.svg' alt='wyhywephoto' width={500} height={500} />
       </div>
       <div className="justify-center flex-1 flex flex-col ite p-0 md:p-0 lg:px-4">
         <h2 className="text-[1.5rem] font-semibold m-0 mb-8 text-left leading-tight lg:text-4xl xl:text-5xl">¿Somos para ti?</h2>
